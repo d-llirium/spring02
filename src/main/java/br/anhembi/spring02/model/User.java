e3d5876cf9ef979e23d21cef2149d3e43afa19f2
d-llirium <plimacarlos@gmail.com>
2022-05-25 21:54:10 -0300

    add gets and sets for each user property

--- src/main/java/br/anhembi/spring02/model/User.java
+++ src/main/java/br/anhembi/spring02/model/User.java
@@ -24,6 +24,40 @@ public class User { // TABELA
     @Column(name = "email", length = 50, unique = true) // coluna chamada email com no máximo 50 caracteres cada, único na tabela
     private String email;
 
-    @Column(name = "senha", length = 30, nullable = false) // coluna com nome se senha de no máximo 30 caracteres e não pode ser nula
+    @Column(name = "senha", length = 30, nullable = false) // coluna com nome se senha de no máximo 30 caracteres e não pode ser nula == @NOTNULL
     private String password;
+
+    // METHODS
+    public long getCode() {
+        return code;
+    }
+
+    public void setCode(long code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
 }
