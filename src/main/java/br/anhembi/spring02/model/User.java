6873759a0f49922b2ee87624ac241affbdbc2693
d-llirium <plimacarlos@gmail.com>
2022-05-25 21:28:58 -0300

    add properties to User entity/class

--- src/main/java/br/anhembi/spring02/model/User.java
+++ src/main/java/br/anhembi/spring02/model/User.java
@@ -1,5 +1,29 @@
 package br.anhembi.spring02.model;
 
-public class User {
-    
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity // indica que esta classe será armazenada no BD
+@Table(name = "usuarios") // sem acento e tudo minúscula
+public class User { // TABELA
+
+    // ATRIBUTES = PROPERTIES = COLUNAS
+
+    @Id // o próximo atributo é uma chave primária PK
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // gerado de forma sequencial, pelo programa, e não se repete
+    @Column(name = "codigo")
+    private long code;
+
+    @Column(name = "nome", length = 120) // coluna chamada nome com no máximo 120 caracteres cada
+    private String name;
+
+    @Column(name = "email", length = 50, unique = true) // coluna chamada email com no máximo 50 caracteres cada, único na tabela
+    private String email;
+
+    @Column(name = "senha", length = 30, nullable = false) // coluna com nome se senha de no máximo 30 caracteres e não pode ser nula
+    private String password;
 }
